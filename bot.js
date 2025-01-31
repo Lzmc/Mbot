07aa965577a2c642e4ad97f6f82d911900150789
Lzmc <143743209+Lzmc@users.noreply.github.com>
2025-01-31 12:46:00 +0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,82 @@
+const mineflayer = require('mineflayer');
+const Movements = require('mineflayer-pathfinder').Movements;
+const pathfinder = require('mineflayer-pathfinder').pathfinder;
+const { GoalBlock, GoalXZ } = require('mineflayer-pathfinder').goals;
+const keep_alive = require('./keep_alive.js')
+
+const config = require('./settings.json');
+
+const loggers = require('./logging.js');
+const logger = loggers.logger;
+
+function createBot() {
+   const bot = mineflayer.createBot({
+      username: config['bot-account']['username'],
+      password: config['bot-account']['password'],
+      auth: config['bot-account']['type'],
+      host: config.server.ip,
+      port: config.server.port,
+      version: config.server.version,
+   });
+
+   bot.loadPlugin(pathfinder);
+   const mcData = require('minecraft-data')(bot.version);
+   const defaultMove = new Movements(bot, mcData);
+   bot.settings.colorsEnabled = false;
+   bot.pathfinder.setMovements(defaultMove);
+
+   bot.once('spawn', () => {
+      logger.info("Bot joined to the server");
+
+      if (config.utils['auto-auth'].enabled) {
+         logger.info('Started auto-auth module');
+
+         let password = config.utils['auto-auth'].password;
+         setTimeout(() => {
+            bot.chat(`/register ${password} ${password}`);
+            bot.chat(`/login ${password}`);
+         }, 500);
+
+         logger.info(`Authentication commands executed`);
+      }
+   });
+
+   bot.on('goal_reached', () => {
+      if(config.position.enabled) {
+         logger.info(
+             `Bot arrived to target location. ${bot.entity.position}`
+         );
+      }
+   });
+
+   bot.on('death', () => {
+      logger.warn(
+         `Bot has been died and was respawned at ${bot.entity.position}`
+      );
+   });
+
+   if (config.utils['auto-reconnect']) {
+      bot.on('end', () => {
+         setTimeout(() => {
+            createBot();
+         }, config.utils['auto-reconnect-delay']);
+      });
+   }
+
+   bot.on('kicked', (reason) => {
+      let reasonText = JSON.parse(reason).text;
+      if(reasonText === '') {
+         reasonText = JSON.parse(reason).extra[0].text
+      }
+      reasonText = reasonText.replace(/§./g, '');
+
+      logger.warn(`Bot was kicked from the server. Reason: ${reasonText}`)
+   }
+   );
+
+   bot.on('error', (err) =>
+      logger.error(`${err.message}`)
+   );
+}
+
+createBot();
